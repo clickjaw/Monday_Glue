6b92b8b57025465d72a2d7a3e4eb6cac402f55ce
Tyler Morgan <clickjaw@outlook.com>
2022-08-08 13:29:42 -0500

    photo

---
+++ mondayglue/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+export default 
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Header/>
+      <Main/>
+      <Footer/>
+      </>
+    )
+  }
+}
---
+++ mondayglue/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default 
+
+class Header extends Component {
+  render() {
+    return (
+
+        <header>
+        <nav>
+        <a href = "#">Home</a>
+        <a href = "#">About</a>
+        <a href = "#">Projects</a>
+        </nav>
+        </header>
+
+    )
+  }
+}
