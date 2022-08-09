2d6ec0afc9af720a3f9ad23389f08c680a32e6ab
Tyler Morgan <clickjaw@outlook.com>
2022-08-09 11:02:02 -0500

    changes

--- mondayglue/src/components/Footer.js
+++ mondayglue/src/components/Footer.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import jsLogo from "../js-32.png";
+import reactLogo from "../logo192.png";
 
 export default class Footer extends Component {
   render() {
     return (
       <footer>
-      ©Tyler Morgan 8/8/2022
+      ©Tyler Morgan 8/8/2022<br/>
+      <p class = "footerLogo">
+      <img class = "footerLogo" src = {jsLogo} width = "40" height = "40"></img>
+
+      <img class = "footerLogo" src = {reactLogo} width = "40" height = "40"></img>
+      </p>
       </footer>
       )
   }
