19f47c2d00da759787063619c792948323a95d90
Tyler Morgan <clickjaw@outlook.com>
2022-08-09 11:41:13 -0500

    scroll

--- mondayglue/src/components/Header.js
+++ mondayglue/src/components/Header.js
@@ -14,7 +14,18 @@ class Header extends Component {
         <span class = "font-link">
         <a class = "one" href = "#">Home</a>
         <a class = "one" href = "#">About</a>
-        <a class = "one" href = "https://github.com/clickjaw?tab=repositories">Projects</a><br/>
+        <div class = "dropdown">
+          <button class = "dropdownButton">Projects</button>
+            <div class = "dropdownContent">
+              <a class = "one" href = "https://clickjaw.github.io/teamProject/">Team Project</a>
+              <a class = "one" href = "https://clickjaw.github.io/soloproject/">Solo Project</a>
+              <a class = "one" href = "https://clickjaw.github.io/todo/">To Do List</a>
+              <a class = "one" href = "https://clickjaw.github.io/TipCalculator-TylerMorgan/">Tip Calculator</a>
+              <a class = "one" href = "https://clickjaw.github.io/SimpleCounter/">Simple Counter</a>
+
+            </div>
+        </div>
+        {/* <a class = "one" href = "https://github.com/clickjaw?tab=repositories">Projects</a><br/> */}
         </span>
         </nav>
         </header>
